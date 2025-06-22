ef810e347274b18dc5060b1c65c39807230f7fab
AlenTwoTime <icjalenz@gmail.com>
2025-06-22 18:16:47 -0500

    Update frontend packages and config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,53 +1,71 @@
 const path = require('path');
+const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash:8].js',
+    chunkFilename: '[name].[contenthash:8].js',
     publicPath: 'auto',
     clean: true,
   },
   mode: 'production',
   devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    port: 8080,
-    historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: process.env.BACKEND_URL || 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    },
-    open: true,
-    hot: true,
-  },
+  // devServer: { /* your existing config */ },
+
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react']
+          }
+        }
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+        use: ['style-loader','css-loader']
+      }
+    ]
   },
+
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js','.jsx'],
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      buffer: require.resolve('buffer/'),
+      process: require.resolve('process/browser'),
+      assert: require.resolve('assert/'),
+      url: require.resolve('url/'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      querystring: require.resolve('querystring-es3'),
+      fs: false, net: false, tls: false
+    }
   },
+
   plugins: [
     new Dotenv(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-    }),
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new NodePolyfillPlugin(),
+    new webpack.IgnorePlugin({ resourceRegExp: /^node:/ }),
+    new webpack.IgnorePlugin({ resourceRegExp: /potrace|sqlite3/ }),
+    new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false })
   ],
+
+  optimization: {
+    splitChunks: { chunks: 'all' },
+    runtimeChunk: 'single'
+  }
 };
