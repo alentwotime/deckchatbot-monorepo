d7ce3f6c08ac51ce0c90d38fec49502876e1c35f
AlenTwoTime <icjalenz@gmail.com>
2025-06-23 06:03:14 -0500

    fix(lib2): update square footage util path

--- libs/lib2/__init__.py
+++ libs/lib2/__init__.py
@@ -1,6 +1,6 @@
-"""Utilities for DeckChatBot."""
+from .square_footage import get_square_footage
 
-__all__ = ["my_helper"]
+__all__ = ["get_square_footage", "my_helper"]
 
 def my_helper(x: int) -> int:
     """Sample helper that doubles the input."""
---
+++ libs/lib2/square_footage.py
@@ -0,0 +1,5 @@
+# libs/lib2/square_footage.py
+
+def get_square_footage(length: float, width: float) -> float:
+    """Simple square footage calculator."""
+    return round(length * width, 2)
